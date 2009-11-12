b6202f982820d6f7dd9cf818b2e8e5e532625264
Matt Ittigson <mattittigson@gmail.com>
2009-11-12 18:11:05 -0500

    Protect against `undefined method `add_acts_as_authentic_module' for ActiveRecord::Base:Class' error while running Rake tasks.

--- lib/authlogic_facebook_connect.rb
+++ lib/authlogic_facebook_connect.rb
@@ -3,6 +3,8 @@ require "authlogic_facebook_connect/acts_as_authentic"
 require "authlogic_facebook_connect/session"
 require "authlogic_facebook_connect/helper"
 
-ActiveRecord::Base.send(:include, AuthlogicFacebookConnect::ActsAsAuthentic)
-Authlogic::Session::Base.send(:include, AuthlogicFacebookConnect::Session)
-ActionController::Base.helper AuthlogicFacebookConnect::Helper
+if ActiveRecord::Base.respond_to?(:add_acts_as_authentic_module)
+  ActiveRecord::Base.send(:include, AuthlogicFacebookConnect::ActsAsAuthentic)
+  Authlogic::Session::Base.send(:include, AuthlogicFacebookConnect::Session)
+  ActionController::Base.helper AuthlogicFacebookConnect::Helper
+end
