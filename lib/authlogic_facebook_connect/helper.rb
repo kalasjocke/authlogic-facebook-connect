c6efd839243732e30f7ecb8e598f3561394e36f4
Michał Szajbe <michal.szajbe@gmail.com>
2009-11-12 11:10:30 +0100

    Now uses << instead of + for string concatenation.
    << is much faster than +.

--- lib/authlogic_facebook_connect/helper.rb
+++ lib/authlogic_facebook_connect/helper.rb
@@ -14,15 +14,15 @@ module AuthlogicFacebookConnect
       end
       
       output = "<form id='connect_to_facebook_form' method='post' action='/#{options[:controller]}'>\n"
-      output += "<input type='hidden' name='authenticity_token' value='#{form_authenticity_token}'/>\n"
-      output += "</form>\n"
-      output += "<script type='text/javascript' charset='utf-8'>\n"
-      output += " function connect_to_facebook() {\n"
-      output += "   #{js_selector}.submit();\n"
-      output += " }\n"
-      output += "</script>\n"
+      output << "<input type='hidden' name='authenticity_token' value='#{form_authenticity_token}'/>\n"
+      output << "</form>\n"
+      output << "<script type='text/javascript' charset='utf-8'>\n"
+      output << " function connect_to_facebook() {\n"
+      output << "   #{js_selector}.submit();\n"
+      output << " }\n"
+      output << "</script>\n"
       options.delete(:controller)
-      output += fb_login_button("connect_to_facebook()", options)
+      output << fb_login_button("connect_to_facebook()", options)
       output
     end
   end
