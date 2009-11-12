35223a0cb04cdf7d85e96cdac4030d0c8c20343c
Michał Szajbe <michal.szajbe@gmail.com>
2009-11-12 11:09:26 +0100

    Now can choose between jquery or prototype when generating code.

--- lib/authlogic_facebook_connect/helper.rb
+++ lib/authlogic_facebook_connect/helper.rb
@@ -4,13 +4,21 @@ module AuthlogicFacebookConnect
       # TODO: Make this with correct helpers istead of this uggly hack.
       
       options[:controller] ||= "user_session"
+      options[:js] ||= :prototype
+      
+      case options[:js]
+      when :prototype
+        js_selector = "$('connect_to_facebook_form')"
+      when :jquery
+        js_selector = "jQuery(#connect_to_facebook_form)"
+      end
       
       output = "<form id='connect_to_facebook_form' method='post' action='/#{options[:controller]}'>\n"
       output += "<input type='hidden' name='authenticity_token' value='#{form_authenticity_token}'/>\n"
       output += "</form>\n"
       output += "<script type='text/javascript' charset='utf-8'>\n"
       output += " function connect_to_facebook() {\n"
-      output += "   $('connect_to_facebook_form').submit();\n"
+      output += "   #{js_selector}.submit();\n"
       output += " }\n"
       output += "</script>\n"
       options.delete(:controller)
